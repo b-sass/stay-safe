d039919e3813222a6941ef94c2113b08fac73cdf
Bartosz Sass <sassbartek@gmail.com>
2025-09-07 15:59:10 +0200

    Created a dialog to add places.

---
+++ app/src/main/java/com/example/staysafe/dialogs/AddPlaceDialog.kt
@@ -0,0 +1,69 @@
+package com.example.staysafe.dialogs
+
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddPlaceDialog(
+    onDismissRequest: () -> Unit
+) {
+    var name by remember { mutableStateOf("") }
+    var latitude: Double? by remember { mutableStateOf(null) }
+    var longitude: Double? by remember { mutableStateOf(null) }
+
+    MessageDialog(
+        onDismissRequest = onDismissRequest,
+        header = "New Place",
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Place Name")}
+            )
+
+            OutlinedTextField(
+                value = latitude?.toString() ?: "",
+                onValueChange = { latitude = it.toDoubleOrNull() },
+                label = { Text("Latitude") }
+            )
+
+            OutlinedTextField(
+                value = longitude?.toString() ?: "",
+                onValueChange = { longitude = it.toDoubleOrNull() },
+                label = { Text("Longitude") }
+            )
+        }
+    }
+}
+
